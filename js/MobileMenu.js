43c3e572c4add3ee2cd4fdac11ecb9a4c2a708f3
alicemirigo92 <alicemirigo92@gmai.com>
2023-01-02 15:07:46 +0300

    Add user clicks on the menu

--- js/MobileMenu.js
+++ js/MobileMenu.js
@@ -1,5 +1,6 @@
 const menu = document.getElementById('mobile-menu');
 const hamburger = document.getElementById('hamburger');
+const menuOptions = document.querySelectorAll('.menu-option')
 
 menu.style.cssText = `
   font-style: poppins, sans-serif;
@@ -7,6 +8,7 @@ menu.style.cssText = `
   list-style: none;
   padding-left: 28px;
   display: none;
+  position: fixed;
 `;
 
 function toggleMenu() {
@@ -15,6 +17,10 @@ function toggleMenu() {
   } else {
     menu.style.display = 'none';
   }
+}
+
+for(let i = 0; i < menuOptions.length; i++){
+  menuOptions[i].addEventListener('click', toggleMenu);
 }
 
 hamburger.addEventListener('click', toggleMenu);
