0c7592055c35c4befde271c37b354527f9006096
Zeph180 <kigozijosephsuubi1@gmail.com>
2023-01-05 17:52:42 +0300

    Fix linter error

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,4 +1,5 @@
-/* eslint-disable no-unused-vars */
+const form = document.querySelector('form');
+
 function validate() {
   const emailInputValue = document.getElementById('email').value;
   const emailTest = /^[a-z0-9_!#$%&'*+=?`{|}~^.-]+@[a-z0-9.-]+$/gm;
@@ -8,6 +9,7 @@ function validate() {
       document.getElementById('emailError').innerHTML = 'Expect amazing projects';
       document.getElementById('emailError').style.visibility = 'visible';
       document.getElementById('emailError').style.color = 'blue';
+      form.submit();
     } else {
       document.getElementById('emailError').innerHTML = 'Emails are case sensitive! please enter the email in lowercase';
       document.getElementById('emailError').style.visibility = 'visible';
@@ -16,3 +18,8 @@ function validate() {
     }
   };
 }
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  validate();
+});
