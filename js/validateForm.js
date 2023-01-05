768798ae801357a9efbd2997ba3ea417afa3bf3a
Zeph180 <kigozijosephsuubi1@gmail.com>
2023-01-05 14:58:43 +0300

    Validate user email input

--- js/validateForm.js
+++ js/validateForm.js
@@ -2,15 +2,16 @@ function validate() {
   const emailInputValue = document.getElementById('email').value;
   const emailTest = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
+  /* eslint-disable no-unused-vars */
   document.forms[0].onsubmit = function (e) {
     if (emailTest.test(emailInputValue)) {
       document.getElementById('emailError').innerHTML = 'Expect amazing projects';
       document.getElementById('emailError').style.visibility = 'visible';
-      document.getElementById('emailError').style.color = 'blue';
+      document.getElementById('emailError').style.color = 'yellow';
     } else {
-      document.getElementById('emailError').innerHTML = 'Emails are case sensitive!! please enter the email in lower case';
+      document.getElementById('emailError').innerHTML = 'Emails are case sensitive! please enter the email in lowercase';
       document.getElementById('emailError').style.visibility = 'visible';
-      document.getElementById('emailError').style.color = 'red';
+      document.getElementById('emailError').style.color = 'purple';
       e.preventDefault();
     }
   };
